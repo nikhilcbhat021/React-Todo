2f3b71589f812727912d281c0d2e4b215e858d07
Nikhil Bhat <nikhilbhat021@gmail.com>
2024-08-19 12:03:02 +0530

    Frontend -- Boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hi There @Author.
+      </div>
+      <div>
+        I'm Nikhil Speaking from India@Jupiter..
+      </div>
+    </>
+  )
+}
+
+export default App
